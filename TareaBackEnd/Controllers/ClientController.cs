9fa1cf7ebfced901e49ef5bcf47c85e87b870753
Vicente Garza Reyna <vicente.garzara@uanl.edu.mx>
2021-02-06 22:20:18 -0600

    Agregar archivos de proyecto.

---
+++ TareaBackEnd/Controllers/ClientController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TareaBackEnd.Models;
+
+namespace TareaBackEnd.Controllers {
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class ClientController : Controller {
+		private NorthwindContext context = new NorthwindContext();
+
+		[HttpGet]
+		private IQueryable<Customer> getCustomersFromDB() {
+			return context.Customers.AsQueryable();
+		}
+
+		public List<Customer> getCustomers() {
+			return getCustomersFromDB().ToList();
+		}
+	}
+}
---
+++ TareaBackEnd/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TareaBackEnd.Models
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
