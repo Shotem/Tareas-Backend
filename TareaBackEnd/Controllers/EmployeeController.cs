daf82cc32adbc9b52c6450bbec5453caa1aca3aa
Vicente Garza Reyna <vicente.garzara@uanl.edu.mx>
2021-03-14 20:59:17 -0600

    Created EmployeeController/Service

---
+++ TareaBackEnd/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TareaBackEnd.Models;
+using TareaBackEnd.Services;
+
+namespace TareaBackEnd.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+
+	public class EmployeeController : Controller {
+		
+		private EmployeeService employeeService = new EmployeeService();
+		
+		[HttpGet]
+		public List<Employee> getEmployees() {
+			return employeeService.getAll().Select( s => new Employee { 
+				EmployeeId = s.EmployeeId,
+				FirstName = s.FirstName,
+				LastName = s.LastName
+			} ).ToList();
+		}
+
+		[HttpGet("{id}")]
+		public Employee getEmployeeByID(int id) {
+			return employeeService.getByID(id);
+		}
+
+		[HttpPost]
+		public void insertEmployee() {
+			var employee = new Employee { };
+			employeeService.insert(employee);
+		}
+
+		[HttpDelete("{id}")]
+		public void deleteEmployeeByID(int id) {
+			employeeService.deleteByID(id);
+		}
+	}
+}
---
+++ TareaBackEnd/Services/EmployeeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TareaBackEnd.Models;
+
+namespace TareaBackEnd.Services {
+	public class EmployeeService : BaseService {
+		private IQueryable<Employee> getFromDB() {
+			return dbContext.Employees;
+		}
+
+		public List<Employee> getAll() {
+			return getFromDB().ToList();
+		}
+
+		public Employee getByID(int id) {
+			return getFromDB().Where(w => w.EmployeeId == id).FirstOrDefault();
+		}
+
+		public void deleteByID(int id) {
+			dbContext.Employees.Remove(getByID(id));
+			dbContext.SaveChanges();
+		}
+
+		public void insert(Employee c) {
+			dbContext.Employees.Add(c);
+			dbContext.SaveChanges();
+		}
+
+	}
+}
